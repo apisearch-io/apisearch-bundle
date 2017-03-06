affe6c5f1388ba0194c568363f04c851927d0e74
Marc Morera <yuhu@mmoreram.com>
2017-03-06 02:58:35 +0100

    Initial commit

---
+++ PuntmigSearchBundle.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Search PHP Bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author PuntMig Technologies
+ */
+
+declare(strict_types=1);
+
+namespace Puntmig\Search;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+use Mmoreram\BaseBundle\SimpleBaseBundle;
+
+/**
+ * Class PuntmigSearchBundle.
+ */
+class PuntmigSearchBundle extends SimpleBaseBundle
+{
+    /**
+     * get config files.
+     *
+     * @return array
+     */
+    public function getConfigFiles() : array
+    {
+        return [
+            'repositories',
+            'query',
+            'twig',
+            'http',
+        ];
+    }
+
+    /**
+     * Return all bundle dependencies.
+     *
+     * Values can be a simple bundle namespace or its instance
+     *
+     * @return array
+     */
+    public static function getBundleDependencies(KernelInterface $kernel) : array
+    {
+        return [
+            FrameworkBundle::class,
+        ];
+    }
+}
---
+++ Twig/QueryExtension.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Search PHP Bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author PuntMig Technologies
+ */
+
+declare(strict_types=1);
+
+namespace Puntmig\Search\Twig;
+
+use Puntmig\Search\Query\UrlBuilder;
+use Twig_Extension;
+use Twig_SimpleFilter;
+
+/**
+ * Class QueryExtension.
+ */
+class QueryExtension extends Twig_Extension
+{
+    /**
+     * @var UrlBuilder
+     *
+     * Url builder
+     */
+    private $urlBuilder;
+
+    /**
+     * QueryExtension constructor.
+     *
+     * @param UrlBuilder $urlBuilder
+     */
+    public function __construct(UrlBuilder $urlBuilder)
+    {
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return Twig_SimpleFilter[] An array of filters
+     */
+    public function getFilters()
+    {
+        return [
+            new Twig_SimpleFilter('add_filter_value', [$this->urlBuilder, 'addFilterValue']),
+            new Twig_SimpleFilter('remove_filter_value', [$this->urlBuilder, 'removeFilterValue']),
+            new Twig_SimpleFilter('remove_price_range_filter', [$this->urlBuilder, 'removePriceRangeFilter']),
+            new Twig_SimpleFilter('add_prev_page', [$this->urlBuilder, 'addPrevPage']),
+            new Twig_SimpleFilter('add_page', [$this->urlBuilder, 'addPage']),
+            new Twig_SimpleFilter('add_next_page', [$this->urlBuilder, 'addNextPage']),
+            new Twig_SimpleFilter('add_sort_by', [$this->urlBuilder, 'addSortBy']),
+        ];
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     *
+     * @deprecated since 1.26 (to be removed in 2.0), not used anymore internally
+     */
+    public function getName()
+    {
+        return 'query_extension';
+    }
+}
