b893f2206fa127933bf4c6d850bb455b54bc0ae2
Marc Morera <yuhu@mmoreram.com>
2018-05-30 18:32:52 +0200

    Added extra token commands

---
+++ Command/AddTokenCommand.php
@@ -0,0 +1,164 @@
+<?php
+
+/*
+ * This file is part of the Search PHP Bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author PuntMig Technologies
+ */
+
+declare(strict_types=1);
+
+namespace Apisearch\Command;
+
+use Apisearch\Token\Token;
+use Apisearch\Token\TokenUUID;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class AddTokenCommand.
+ */
+class AddTokenCommand extends WithAppRepositoryBucketCommand
+{
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('apisearch:add-token')
+            ->setDescription('Add a token')
+            ->addArgument(
+                'uuid',
+                InputArgument::REQUIRED,
+                'UUID'
+            )
+            ->addArgument(
+                'repository',
+                InputArgument::REQUIRED,
+                'Repository name'
+            )
+            ->addOption(
+                'index',
+                null,
+                InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL,
+                'Indices',
+                []
+            )
+            ->addOption(
+                'http-referrer',
+                null,
+                InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL,
+                'Http referrers',
+                []
+            )
+            ->addOption(
+                'endpoint',
+                null,
+                InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL,
+                'Endpoints',
+                []
+            )
+            ->addOption(
+                'plugin',
+                null,
+                InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL,
+                'Plugins',
+                []
+            )
+            ->addOption(
+                'seconds-valid',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Seconds valid',
+                Token::INFINITE_DURATION
+            )
+            ->addOption(
+                'max-hits-per-query',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Maximum hits per query',
+                Token::INFINITE_HITS_PER_QUERY
+            )
+            ->addOption(
+                'ttl',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'TTL',
+                Token::DEFAULT_TTL
+            );
+    }
+
+    /**
+     * Dispatch domain event.
+     *
+     * @return string
+     */
+    protected function getHeader(): string
+    {
+        return 'Add token';
+    }
+
+    /**
+     * Get success message.
+     *
+     * @param InputInterface $input
+     * @param mixed          $result
+     *
+     * @return string
+     */
+    protected function getSuccessMessage(
+        InputInterface $input,
+        $result
+    ): string {
+        return sprintf(
+            'Token with UUID <%s> added properly',
+            $input->getArgument('uuid')
+        );
+    }
+
+    /**
+     * Dispatch domain event.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return mixed|null
+     */
+    protected function runCommand(InputInterface $input, OutputInterface $output)
+    {
+        $repository = $input->getArgument('repository');
+        $indexArray = $this
+            ->repositoryBucket
+            ->getConfiguration()[$repository]['indexes'] ?? [];
+
+        $indices = array_map(function (string $index) use ($indexArray) {
+            return $indexArray[$index] ?? null;
+        }, $input->getOption('index'));
+        $indices = array_filter($indices);
+
+        $this
+            ->repositoryBucket->findRepository($repository)
+            ->addToken(
+                new Token(
+                    TokenUUID::createById($input->getArgument('uuid')),
+                    (string) $input->getArgument('repository'),
+                    $indices,
+                    $input->getOption('http-referrer'),
+                    $input->getOption('endpoint'),
+                    $input->getOption('plugin'),
+                    (int) $input->getOption('seconds-valid'),
+                    (int) $input->getOption('max-hits-per-query'),
+                    (int) $input->getOption('ttl')
+                )
+            );
+    }
+}
